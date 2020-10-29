9245bf6e11c2915775d2f62f6e3b97c442b849a0
Codrean <ioanacodrean@yahoo.com>
2020-10-29 10:02:39 +0100

    edit
    the mane of folder img

--- css-animations-with-html-forms-05-master/index.php
+++ css-animations-with-html-forms-05-master/index.php
@@ -1,35 +1,35 @@
 <?php
 /*
-* Plugin Name: Owl Popup 
-* Plugin URI: https://storyscaping.codrean.dk
-* Description: TH. LANGS counselor popup for frontpage 
-* Version: 0.9.9
-* Author: Pernille Graff, Bodil Fomsgaard, Branko Vlahov & Ioana Codrean
-* Author URI: https://storyscaping.codrean.dk
+* Plugin Name: Owl 
+* Plugin URI: http://localhost/learningwordpress/cheers
+* Description: This is a Newsletter Signup form Plugin for Cheers based on HTML5, CSS, JS and PHP
+* Version: 2.9.9
+* Author: Murat Kilic
+* Author URI: http://localhost/learningwordpress/cheers
 * License: GPL2
 */
 
-function owlpopup_form()
+function newsletter_form()
 {
     $content = '';
-    $content .= '<div class="popup-form">';
+    $content .= '<div class="login-form">';
     $content .= '<div class="popupCloseButton">X</div>';
-    $content .= '<img id="owl" src=" '.plugins_url("popup/img/owl.png").' " ';
-    $content .= 'alt="TH: LANGS owl mascot">';
   	$content .= '<section>';
-    $content .= '<h3 id="velkommen">Are you new to TH. LANGS?</h3>';
-  	$content .= '<h5 id="tilmeld">write your name and email to get in touch with our skilled study counselors who can help you find your future</h5>';
+    $content .= '<h3 id="velkommen">Welcome TO </h3>';
+    $content .= '<img id="logo" src=" '.plugins_url("partyvibesplugin/img/logo.png").' " ';
+    $content .= 'alt="Cheers Logo">';
+  	$content .= '<h5 id="tilmeld">Sign up for our weekly partyvibes</h5>';
   	$content .= '</section>';
     $content .= '<section class="form">';
     $content .= '<form action="#" id="myForm">';
     $content .= '<div class="input">';
-    $content .= '<input type="text" name="email" id="email" placeholder="Enter your name here..." required>';
+    $content .= '<input type="email" name="email" id="email" placeholder="Enter your name here..." required><i class="fas fa-envelope"></i>';
     $content .= '</div>';
   	$content .= '<div>';
-    $content .= '<input type="email" name="email" id="email" placeholder="Enter your e-mail here..." required>';
+ 	$content .= '<input type="submit" value="LET THE GAME BEGIN!" name="submitBtn" id="submitBtn2">';
     $content .= '</div>';
   	$content .= '<div>';
- 	$content .= '<input type="button" value="begin your future" name="submitBtn" id="submitBtn">';
+ 	$content .= '<input type="button" value="No, I\'m boring" name="submitBtn" id="submitBtn">';
     $content .= '</div>';
     $content .= '</form>';
     $content .= '</section>';
@@ -38,7 +38,7 @@ function owlpopup_form()
 }
 
     #First parameter is a self choosen name for a unique short-code. Second parameter is the name of the function that creates the newsletter
-    add_shortcode('show_owlpopup','owlpopup_form');
+    add_shortcode('show_partyvibesplugin','newsletter_form');
     
     add_action('wp_enqueue_scripts','register_styles_and_scripts_for_plugin');
     
@@ -49,13 +49,11 @@ function owlpopup_form()
         
         wp_enqueue_style('CustomFontMontserrat','https://fonts.googleapis.com/css?family=Montserrat:400,500,800&display=swap');
         
-        wp_enqueue_style('CustomStylesheet', plugins_url('popup/css/style.css'));
+        wp_enqueue_style('CustomStylesheet', plugins_url('partyvibesplugin/css/style.css'));
         
         wp_deregister_script('jquery');
         
         wp_enqueue_script('jquery','https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js', array(), null, true);
         
-        wp_enqueue_script('CustomScript', plugins_url('popup/js/script.js'), array('jquery'), null, true);
+        wp_enqueue_script('CustomScript', plugins_url('partyvibesplugin/js/script.js'), array('jquery'), null, true);
     }
-
-
